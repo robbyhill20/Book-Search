27f421b5d42e97d297b8abdf2f28c79664f05d09
Robby Hill <88348635+robbyhill20@users.noreply.github.com>
2022-03-08 15:57:12 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+require('dotenv').config()
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -15,12 +18,13 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
